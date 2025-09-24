b922c353d8421f8d96e4139c6fbccd9c0d3387da
cydrickearlbuhia0714-jpg <cydrickearlbuhia0714@gmail.com>
2025-09-24 10:56:32 +0800

    Add files via upload

---
+++ BUHIA.cpp.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+     cout << "SPIDER MAN LOGO:   \n\n" << endl;
+     cout << "      o  o    o  o        " << endl; 
+	 cout << "      o o      o o        " << endl;
+	 cout << "     o o        o o       " << endl;
+	 cout << "     o o        o o       " << endl;
+	 cout << "    oo o        o oo      " << endl; 
+	 cout << "    oo o        o oo      " << endl;
+	 cout << "    oo  oo oo oo  oo      " << endl;
+	 cout << "    oooo oooooo oooo     _____ ____  _____ ____  _____ _____ " << endl;
+	 cout << "    oo  o oooo o  oo     |     |   |   |   |   | |     |   |" << endl;
+	 cout << "      oo oooooo oo       |____ |___|   |   |   | |__   |___|" << endl;
+	 cout << "   o o  o oooo o  o o        | |       |   |   | |     |   \\" << endl;
+	 cout << "   oo   o oooo o   oo    ____| |     __|__ |___| |____ |    \\" << endl;
+	 cout << "   oo   o  oo  o   oo                 _____ " << endl;
+	 cout << "   oo   o  oo  o   oo       |\\   /|  |     |  |\\    |            " << endl;
+     cout << "   oo   o      o   oo       | \\ / |  |     |  | \\   |            " << endl;
+	 cout << "   oo   o      o   oo       |  V  |  |_____|  |  \\  |            " << endl;
+	 cout << "    o   o      o   o        |     |  |     |  |   \\ |            " << endl; 
+	 cout << "    o   oo    oo   o        |     |  |     |  |    \\|            " << endl;
+	 cout << "     o   o    o   o       " << endl;  
+	 cout << "     o   o    o   o       " << endl;
+	 cout << "      o  o    o  o        " << endl;
+	 cout << "         o    o           " << endl;
+	 cout << "          o  o            " << endl;
+
+}
